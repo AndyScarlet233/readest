39b828e4d40f49d84887be3239fe77ca1f8d84f6
Andy Scarlet <248809439+AndyScarlet233@users.noreply.github.com>
2026-09-13 18:39:30 +0800

    chore: align Calibre plugin core version with 0.12.8

--- apps/readest-calibre-plugin/__init__.py
+++ apps/readest-calibre-plugin/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = '2026, Bilingify LLC'
 
 from calibre.customize import InterfaceActionBase
 
-PLUGIN_VERSION = (0, 11, 20)
+PLUGIN_VERSION = (0, 12, 8)
 
 
 class ReadestPlugin(InterfaceActionBase):
